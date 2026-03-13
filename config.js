727b543d85182097b73fdb9971b8eab9d3a35fb9
profesionaldeveolpers-art <profesionaldeveolpers@gmail.com>
2026-03-13 17:59:26 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.siteConfig = {
-  whatsappNumber: '529984062878', // Reemplaza con el número real en formato internacional
-  email: 'roopharq@gmail.com',       // Reemplaza con el correo real
-  driveUrl: 'https://drive.google.com/', // Reemplaza con el link real del portafolio
-  whatsappMessage: 'Hola, me gustaría recibir información sobre su trabajo.'
-};
